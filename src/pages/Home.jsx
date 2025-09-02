20dbea52f0dc01e9e2c709a947dd5e05d7a9f442
andresanni <andres.anni1985@gmail.com>
2025-09-02 18:40:15 -0300

    First Video

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import BottomNav from "../components/shared/BottomNav";
+
+const Home = () => {
+  return (
+    <section className="bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
+      {/* LEFT DIV */}
+      <div className="flex-[3] bg-[#1a1a1a]">
+
+      </div>
+      {/* RIGHT DIV */}
+      <div className="flex-[2] bg-[#1a1a1a]">
+        <BottomNav />
+      </div>
+    </section>
+  )
+}   
+export default Home;
